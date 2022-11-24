ce934b2ad5c4a6afa2d908260e25768ab1395738
RBrooks76 <104989421+RBrooks76@users.noreply.github.com>
2022-11-25 01:17:59 +1000

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <prediction/>
+  </div>
+</template>
+
+<script>
+
+
+import Prediction from "@/views/Predictions/Prediction";
+
+
+export default {
+  name: 'Home',
+  components: {Prediction},
+}
+</script>
+<style lang="scss">
+
+</style>
